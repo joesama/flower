ca12b5317b2fba72a85f6b07f5cd2c92e84bc728
Johari <joharijumali@gmail.com>
2017-09-18 22:05:18 +0800

    add flow n step reference

--- src/Data/Repositories/FlowerApi.php
+++ src/Data/Repositories/FlowerApi.php
@@ -3,6 +3,7 @@ namespace Joesama\Flower\Data\Repositories;
 
 use Joesama\Flower\Data\Model\Flow;
 use Joesama\Flower\Data\Model\Step;
+use Joesama\Flower\Http\Processor\StepsManager;
 
 /**
  * Data API for flower
@@ -30,11 +31,38 @@ class FlowerApi
 	 * @return void
 	 * @author 
 	 **/
-	public function getSteps()
+	public function getFirstSteps($exclude = [])
 	{
-		return Step::all();
+		return Step::whereNull('fk_jfs_step')->whereNotIn('id',$exclude)->get();
 	}
 
+
+	/**
+	 * Get All Steps For Specific Flow 
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public function getFlowSteps($flowCode)
+	{
+		$manager = new StepsManager($flowCode);
+
+		return $manager->registeredStep();
+
+		return Step::where('fk_jf_flow',$flowId)->get();
+	}
+
+
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	public function getStepInfo($id)
+	{
+		return Step::find($id);
+	}
 
 } // END class FlowerApi 
 	
